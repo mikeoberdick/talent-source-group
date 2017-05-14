fb48782829a4b257095bf25e1357b89a79a56da0
Mike Oberdick <mikeoberdick@gmail.com>
2017-05-14 19:31:59 -0400

    Homepage, Header, Nav, Footer

--- page.php
+++ page.php
@@ -12,50 +12,35 @@
 
 get_header();
 
-$container   = get_theme_mod( 'understrap_container_type' );
-$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
-
 ?>
 
 <div class="wrapper" id="page-wrapper">
 
-	<div class="<?php echo esc_html( $container ); ?>" id="content" tabindex="-1">
+	<?php while ( have_posts() ) : the_post(); ?>
 
-		<div class="row">
+	<?php
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check', 'none' ); ?>
+	if ( is_page( 'homepage' ) ) {
+		get_template_part( 'template-parts/content', 'home' );
+	}
 
-			<main class="site-main" id="main">
+	else {
+	get_template_part( 'loop-templates/content', 'page' );
+	}
 
-				<?php while ( have_posts() ) : the_post(); ?>
+	?>
 
-					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+	<?php
+	// If comments are open or we have at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) :
+	comments_template();
+	endif;
+	?>
 
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
-
-				<?php endwhile; // end of the loop. ?>
-
-			</main><!-- #main -->
-
-		</div><!-- #primary -->
-
-		<!-- Do the right sidebar check -->
-		<?php if ( 'right' === $sidebar_pos || 'both' === $sidebar_pos ) : ?>
-
-			<?php get_sidebar( 'right' ); ?>
-
-		<?php endif; ?>
-
-	</div><!-- .row -->
-
-</div><!-- Container end -->
+	<?php endwhile; // end of the loop. ?>
 
 </div><!-- Wrapper end -->
+
+<div id="js-heightControl" style="height: 0;">&nbsp;</div>
 
 <?php get_footer(); ?>
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -1,4 +1,9 @@
-<?php
-//custom homepage content here
-echo 'Add some custom content!'; 
-?>
+<div class="container heroSection mb-5" style = "background-image: url('<?php the_field('hero_image'); ?>')">
+	<div id = "opacityLayer">
+		<div class="row">
+			<div id = "hpCTA" class = "col-sm-12 text-center mt-5 mb-5">
+				<h1>HEY NOW</h1>
+			</div>		
+		</div><!-- .row -->
+	</div><!-- #opacityLayer -->
+</div><!-- .container -->
