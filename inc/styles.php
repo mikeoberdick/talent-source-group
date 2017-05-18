034a2bb1a2ea5ce4a0fb62dffb5bfd28d0aa6f0b
Mike Oberdick <mikeoberdick@gmail.com>
2017-05-17 20:26:26 -0400

    Added Employers, Candidates, Single Jobs (Modal Application)

--- inc/styles.php
+++ inc/styles.php
@@ -10,5 +10,6 @@ function d4tw_enqueue_styles () {
     wp_enqueue_style( 'Open Sans', 'https://fonts.googleapis.com/css?family=Open+Sans' );
     wp_enqueue_style( 'Slabo', 'https://fonts.googleapis.com/css?family=Slabo+13px' );
     wp_enqueue_style( 'Cabin', 'https://fonts.googleapis.com/css?family=Cabin' );
+    wp_enqueue_style( 'Featherlight CSS', get_stylesheet_directory_uri() . '/fl/featherlight.css' );
 }
 add_action('wp_enqueue_scripts', 'd4tw_enqueue_styles');
