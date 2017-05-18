69f665fe2e6cf25e4d8e7224c680798a38621587
Mike Oberdick <mikeoberdick@gmail.com>
2017-05-18 19:23:12 -0400

    Updated Application Process

--- inc/styles.php
+++ inc/styles.php
@@ -7,7 +7,6 @@
 
 // Add the theme styles
 function d4tw_enqueue_styles () {
-    wp_enqueue_style( 'Open Sans', 'https://fonts.googleapis.com/css?family=Open+Sans' );
     wp_enqueue_style( 'Slabo', 'https://fonts.googleapis.com/css?family=Slabo+13px' );
     wp_enqueue_style( 'Cabin', 'https://fonts.googleapis.com/css?family=Cabin' );
     wp_enqueue_style( 'Featherlight CSS', get_stylesheet_directory_uri() . '/fl/featherlight.css' );
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -1,17 +1,18 @@
 <div class="container heroSection mb-5" style = "background-image: url('<?php the_field('hero_image'); ?>')">
 	<div class="row">
 			<div class = "opacityLayer">
-				<div class = "col-sm-12 mt-5">
-					<h2 class = "mt-5 mb-5 text-center pl-5 pr-5 hpCTA"><?php the_field('homepage_cta'); ?></h2>
+				<div class = "col-sm-12 hpCallout">
+					<h2 class = "mb-5 text-center pl-5 pr-5 hpCTA"><?php the_field('homepage_cta'); ?></h2>
+
+						<div class ="row hpButtons">
+							<div class = "col-sm-6 text-center">
+								<a href = "<?php echo bloginfo('url'); ?>/candidates"><button type="button" class="btn btn-primary btn-lg">Get A Job</button></a>
+							</div>
+							<div class = "col-sm-6 text-center">
+								<a href = "<?php echo bloginfo('url'); ?>/employers"><button type="button" class="btn btn-primary btn-lg">List A Job</button></a>
+							</div>
+						</div><!-- .row -->
 				</div>
-				<div class="row hpButtons">
-			<div class = "col-sm-4 offset-sm-1">
-				<a href = "<?php echo bloginfo('url'); ?>/candidates"><button type="button" class="btn btn-primary btn-lg">Get A Job</button></a>
-			</div>
-			<div class = "col-sm-4">
-				<a href = "<?php echo bloginfo('url'); ?>/employers"><button type="button" class="btn btn-primary btn-lg">List A Job</button></a>
-			</div>
-		</div><!-- .row -->
 			</div><!-- #opacityLayer -->	
 		</div><!-- .row -->
 </div><!-- .container-fluid -->
