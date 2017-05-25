1f6026466ec0565147739b375930ce307a3ee626
Mike Oberdick <mikeoberdick@gmail.com>
2017-05-25 09:40:09 -0400

    Homepage Updates

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -24,9 +24,9 @@
 	<div class="container text-center">
 		<div class="row">
 			<div class="col-sm-12 ">
-				<h3 class = "sectionHeader">Solutions</h3>
+				<h3 class = "sectionHeader mb-5">Solutions</h3>
 			</div><!-- .col-sm-12 -->
-				<div class="col-sm-6 mt-5 mb-3">
+				<div class="col-sm-6 mb-3">
 					<i class="fa fa-handshake-o mb-3" aria-hidden="true"></i>
 					<h3>Permanent Placement</h3>
 				</div><!-- .col-sm-6 -->
@@ -45,10 +45,10 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12">
-				<h3 class = "sectionHeader">Representative Areas</h3>
+				<h3 class = "sectionHeader mb-5">Representative Areas</h3>
 			</div><!-- .col-sm-12 -->
 			
-			<div class="col-sm-6 mt-5 mb-3">
+			<div class="col-sm-6 mb-3">
 				<?php the_field('practice_areas_left'); ?>
 			</div><!-- .col-sm-6 -->
 
@@ -60,13 +60,26 @@
 	</div><!-- .container -->
 </div><!-- .container-fluid -->
 
-<div class="container-fluid" id="ctaSection">
+<div class="container-fluid" id="linkedIn">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 content">
-				<h3>Let's Talk</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sunt ea minima!</p>
-						<a class = "btn btn-primary btn-large" href="<?php echo bloginfo('url'); ?>/contact">Contact Talent Source Group</a>
+				<h3 class = "sectionHeader mb-5">LinkedIn Content Here</h3>
+	<a class = "btn btn-primary btn-large mt-5" href="#">Check out TSG on Linked IN</a>
+			</div><!-- .col-sm-12 -->
+		</div><!-- .row -->
+	</div><!-- .container -->
+</div><!-- #ctaSection.container-fluid -->
+
+<div class="container-fluid" id="ctaSection">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div id = "ctaWrapper">
+					<h3>Let's Talk</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sunt ea minima!</p>
+							<a class = "btn btn-primary btn-large" href="<?php echo bloginfo('url'); ?>/contact">Contact Talent Source Group</a>
+				</div>
 			</div><!-- .col-sm-12 -->
 		</div><!-- .row -->
 	</div><!-- .container -->
